3768924778e4670634e5514525f20ea9aac3f33a
Nothinel <nothinel@hotmail.de>
2021-10-30 17:04:36 +0200

    Fixed minor typos, started to conform to PEP8

--- plots.py
+++ plots.py
@@ -2,9 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df=pd.read_table('Cu.dat', comment='#', delim_whitespace=(True),
-names=['e', 'xmu', 'bkg', 'pre_edge', 'post_edge',
-'der', 'sec', 'i0', 'chie'])
+df=pd.read_table(
+        'Cu.dat', comment='#', delim_whitespace=(True), 
+        names=[
+            'e', 'xmu', 'bkg', 'pre_edge', 'post_edge', 'der', 'sec', 'i0',
+            'chie'
+            ]
+        )
 axesy = 'derivative'
 
 df['derivative']= df.xmu.diff(periods=-1)/df.e.diff(periods=-1)
@@ -19,5 +23,5 @@ plt.axvline(x=kedge, color='red')
 plt.xlim([kedge-50,kedge+50])
 
 print(kedge)
-plt.savefig(plot.png)
+plt.savefig("plot.png")
 plt.show()
