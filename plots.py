449d3855402893961fff6c5d6ab936118bb5f367
Nothinel <nothinel@hotmail.de>
2021-10-30 18:05:49 +0200

    Typo fix

--- plots.py
+++ plots.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 df=pd.read_table(
-        'Cu.dat', comment='#', delim_whitespace=(True), 
+        'Cu.dat', comment='#', delim_whitespace=True, 
         names=[
             'e', 'xmu', 'bkg', 'pre_edge', 'post_edge', 'der', 'sec', 'i0',
             'chie'
@@ -13,7 +13,7 @@ axesy = 'derivative'
 
 df['derivative']= df.xmu.diff(periods=-1)/df.e.diff(periods=-1)
 
-kedge = x=df.e[df.derivative.argmax()]
+kedge = df.e[df.derivative.idxmax()]
 
 df.plot(x='e', y=['der', 'derivative'])
 plt.xlabel('Energy in [\si(\electronvolt)]')
@@ -24,4 +24,4 @@ plt.xlim([kedge-50,kedge+50])
 
 print(kedge)
 plt.savefig("plot.png")
-plt.show()
+#plt.show()
