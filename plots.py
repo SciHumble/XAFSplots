d82aa7fd5f80c63cf9a35899389ec161b9b2ec4e
SciHumble <93215458+SciHumble@users.noreply.github.com>
2021-11-07 22:08:09 +0100

    Update plots.py

--- plots.py
+++ plots.py
@@ -27,15 +27,14 @@ ax.set_xlabel('Energy (eV)')
 ax.set_ylabel('Absorption')
 ax.grid(axis='both')
 ax.axvline(x=Kedge_Theory, color='red', linewidth=1)
+#Der Text wird nicht angezeigt
 ax.text(x=Kedge_Theory + 5, y=df.der.idxmax(), s=str(Kedge_Theory))
 ax.set_xlim([Kedge_Theory - 50, Kedge_Theory + 50])
 
 for i in range(0, len(Reference_Points)):
     ax.axvline(x=Reference_Points[i], color='blue', linewidth=0.5)
+#Der Text wird nicht angezeigt
     ax.text(x=Reference_Points[i] + 5, y=df.der.idxmax() + 10, s=str(Reference_Points[i]))
 
 filename = '%s.svg' % Element
-plt.savefig(filename)        
-        
-print(KedgeInDataFrame)
-plt.savefig("plot.png")
+plt.savefig(filename)       
