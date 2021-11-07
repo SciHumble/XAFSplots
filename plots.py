ba1628855bb57efb4a2fee6574818b7845e4b719
SciHumble <93215458+SciHumble@users.noreply.github.com>
2021-11-07 22:01:14 +0100

    Update plots.py

--- plots.py
+++ plots.py
@@ -2,26 +2,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+#Vorbereitung um das alles in eine Funktion plot(Element, Kedge_Theory, *Reference_Points) zu integrieren.
+Element = 'Cu'
+Reference_Points = (8984.5, 8989.6)
+Kedge_Theory = 8979
+
+datafile = '%s.dat' % Element
+
 df=pd.read_table(
-        'Cu.dat', comment='#', delim_whitespace=True, 
+        datafile, comment='#', delim_whitespace=True, 
         names=[
             'e', 'xmu', 'bkg', 'pre_edge', 'post_edge', 'der', 'sec', 'i0',
             'chie'
             ]
         )
-axesy = 'derivative'
 
-df['derivative']= df.xmu.diff(periods=-1)/df.e.diff(periods=-1)
+KedgeInDataFrame = df.e[df.der.idxmax()]
+XOffset = Kedge_Theory - KedgeInDataFrame
 
-kedge = df.e[df.derivative.idxmax()]
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.plot(df.e + XOffset, df.der)
+ax.set_xlabel('Energy (eV)')
+ax.set_ylabel('Absorption')
+ax.grid(axis='both')
+ax.axvline(x=Kedge_Theory, color='red', linewidth=1)
+ax.text(x=Kedge_Theory + 5, y=df.der.idxmax(), s=str(Kedge_Theory))
+ax.set_xlim([Kedge_Theory - 50, Kedge_Theory + 50])
 
-df.plot(x='e', y=['der', 'derivative'])
-plt.xlabel('Energy in [\si(\electronvolt)]')
-plt.ylabel('Absorption')
-plt.grid(axis='both')
-plt.axvline(x=kedge, color='red')
-plt.xlim([kedge-50,kedge+50])
+for i in range(0, len(Reference_Points)):
+    ax.axvline(x=Reference_Points[i], color='blue', linewidth=0.5)
+    ax.text(x=Reference_Points[i] + 5, y=df.der.idxmax() + 10, s=str(Reference_Points[i]))
 
-print(kedge)
+filename = '%s.svg' % Element
+plt.savefig(filename)        
+        
+print(KedgeInDataFrame)
 plt.savefig("plot.png")
-#plt.show()
